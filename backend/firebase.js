bbfdbc376071772dde090f6817ea1c3fe87e7051
Jackson Costa <jacksoncostaleites2017@gmail.com>
2025-01-11 22:43:59 -0300

    first commit

---
+++ backend/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIZKgSz_t8F3fjkmdthKmwyMZflVQVnEg",
+  authDomain: "anota-54767.firebaseapp.com",
+  projectId: "anota-54767",
+  storageBucket: "anota-54767.firebasestorage.app",
+  messagingSenderId: "183749902008",
+  appId: "1:183749902008:web:b6e741ff24f23f8a503e4d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
