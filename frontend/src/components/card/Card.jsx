d35613546a422062996f06d8d0e01868cd7e5840
Jackson Costa <jacksoncostaleites2017@gmail.com>
2025-01-21 01:50:53 -0300

    Updated

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,19 +1,135 @@
 import "./card.css";
+import { useState, useEffect, useRef } from "react";
 
-export default function Card({ title, content, priority, status, type, date }) {
+export default function Card({
+  id,
+  title,
+  content,
+  priority,
+  status,
+  type,
+  date,
+}) {
+  const [priorityColor, setPriorityColor] = useState("");
+  const [typeIcon, setTypeIcon] = useState("");
+  const [noteType, setNoteType] = useState("");
+  const [checkBtnVisibility, setCheckBtnVisibility] = useState("hide");
+  const [modalOptionsIsShow, setModalOptionsIsShow] = useState(false);
+
+  const modalOptionsRef = useRef(null);
+  const btnOptionsRef = useRef(null);
+
+  useEffect(() => {
+    switch (priority) {
+      case "low":
+        setPriorityColor("green");
+        break;
+      case "medium":
+        setPriorityColor("orange");
+        break;
+      case "high":
+        setPriorityColor("red");
+        break;
+      default:
+        setPriorityColor("blue");
+        break;
+    }
+  }, [priority]);
+
+  useEffect(() => {
+    switch (type) {
+      case "task":
+        setTypeIcon("bi bi-ui-checks");
+        break;
+      case "note":
+        setTypeIcon("bi bi-sticky-fill");
+        break;
+      default:
+        setTypeIcon("bi bi-x-octagon-fill");
+        break;
+    }
+  }, [type]);
+
+  useEffect(() => {
+    switch (type) {
+      case "task":
+        setNoteType("Tarefa");
+        break;
+      case "note":
+        setNoteType("Anotação");
+        break;
+      default:
+        setNoteType("Desconhecido");
+        break;
+    }
+  }, [type]);
+
+  useEffect(() => {
+    if (status === "todo" && type === "task") {
+      setCheckBtnVisibility("show");
+    } else {
+      setCheckBtnVisibility("hide");
+    }
+  }, [status]);
+
+  const handleOptions = () => {
+    if (modalOptionsIsShow) {
+      modalOptionsRef.current.classList.remove("hide");
+      modalOptionsRef.current.classList.add("show");
+      setModalOptionsIsShow(false);
+    } else {
+      setModalOptionsIsShow(true);
+      modalOptionsRef.current.classList.remove("show");
+      modalOptionsRef.current.classList.add("hide");
+    }
+  };
+
   return (
     <div className="card">
-      <div className="card-priority"></div>
+      <button
+        className="btn-options"
+        type="button"
+        onClick={handleOptions}
+        ref={btnOptionsRef}
+      >
+        <i className="bi bi-three-dots-vertical"></i>
+      </button>
+
+      <div
+        className={`options-modal ${modalOptionsIsShow}`}
+        ref={modalOptionsRef}
+      >
+        <button>
+          <i className="bi bi-pencil-fill"></i>
+          Editar
+        </button>
+        <button>
+          <i className="bi bi-trash-fill"></i>
+          Excluir
+        </button>
+      </div>
+
+      <div className={`card-priority ${priorityColor}`}></div>
       <h2>{title}</h2>
       <p className="content">{content}</p>
-      <p className="date">
-        <i class="bi bi-calendar2-fill"> </i>
-        {date}
-      </p>
-      <p className="type">
-        <i class="bi bi-file-earmark-text-fill"></i>
-        {type}
-      </p>
+
+      <div className="card-footer">
+        <p className="date">
+          <i className="bi bi-calendar2-fill"> </i>
+          {date}
+        </p>
+
+        <div className="card-type">
+          <p className="type">
+            <i className={typeIcon}></i>
+            {noteType}
+          </p>
+
+          <button className={`check-btn-${checkBtnVisibility}`} type="button">
+            <i className="bi bi-check"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -80,10 +80,30 @@ export default function Home() {
             content={
               "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
             }
-            priority={"low"}
+            priority={"medium"}
             status={"todo"}
-            type={"Tarefa"}
+            type={"task"}
             date={"19/01/2025"}
+          />
+          <Card
+            title={"Teste de título"}
+            content={
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+            priority={"high"}
+            status={"complete"}
+            type={"task"}
+            date={"20/01/2025"}
+          />
+          <Card
+            title={"Teste de título"}
+            content={
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+            }
+            priority={""}
+            status={""}
+            type={"note"}
+            date={"20/01/2025"}
           />
         </div>
       </div>
