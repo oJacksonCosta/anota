a7cc92634c38256dfbdf19b7a384c65637b5128c
Jackson Costa <jacksoncostaleites2017@gmail.com>
2025-01-30 16:41:25 -0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
